5adb3168852942a87ab6bc7f506afd012094e4c2
Sethu Kumar <csksethu111@gmail.com>
2026-01-30 23:46:33 +0530

    [QuantumCookie] Blendspaces and Character assets
    - Setup basic blendspace and anim layers
    - Basic mask class
    - Imported ui sprites and character models

---
+++ GGJ/Assets/Scripts/Masks/Mask.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Masks
+{
+    public delegate void MaskEvent();
+    
+    public abstract class Mask : MonoBehaviour
+    {
+        public event MaskEvent OnMaskActivated;
+        public event MaskEvent OnMaskDeactivated;
+
+        private bool _isMaskActive = false;
+        
+        public virtual void Activate()
+        {
+            _isMaskActive = true;
+            OnMaskActivated?.Invoke();
+        }
+
+        public virtual void Deactivate()
+        {
+            _isMaskActive = false;
+            OnMaskDeactivated?.Invoke();
+        }
+        
+        public bool IsMaskActive() => _isMaskActive;
+    }
+}
---
+++ GGJ/Assets/Scripts/Masks/MeleeMask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace Masks
+{
+    public class MeleeMask : Mask
+    {
+        [SerializeField] private float meleeDelay = 0.1f;
+        
+        private float meleeTimer = 0f;
+        
+        public override void Activate()
+        {
+            base.Activate();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (meleeTimer >= meleeDelay)
+                {
+                    meleeTimer = 0f;
+                    ExecuteMeleeAttack();
+                }
+                
+                meleeTimer += Time.deltaTime;
+            }
+        }
+
+        private void ExecuteMeleeAttack()
+        {
+            //TODO: Stuff on player character anim and stuff
+        }
+
+        public override void Deactivate()
+        {
+            base.Deactivate();
+        }
+    }
+}
